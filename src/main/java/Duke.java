fe063477428736866963d17027aad7b701a1a08e
josephhhhhhhhh <e0406453@u.nus.edu>
2020-08-26 04:35:06 +0800

    Level 2. Add, List has been implemented.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
+        int orderAdded = 1;
+        String[] storedItems = new String[100];
         String awaitCommand;
-        boolean noBye = true;
+        //boolean noBye = true;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+
+
+
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
@@ -17,13 +23,24 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
         Scanner in = new Scanner(System.in);
-        while(noBye) {
-            awaitCommand = in.nextLine();
-            if(awaitCommand.equals("bye")){
+        
+        while(true) {
+            awaitCommand = in.nextLine();   //awaits user input for the command
+            if(awaitCommand.equals("list")){        //command list lists out all recorded words
+                for(int i=0; i<(orderAdded-1); i++){
+                    System.out.println(storedItems[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+            if(awaitCommand.equals("bye")){ //command bye breaks the loop and ends the interaction with dukebot
                 break;
             }
             System.out.println("____________________________________________________________");
+            System.out.print("added: ");
             System.out.println(awaitCommand);
+            storedItems[orderAdded-1] = orderAdded + ". " + awaitCommand; //adds the item into the string array with order no.
+            orderAdded++;
             System.out.println("____________________________________________________________");
 
         }
