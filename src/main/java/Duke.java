c3a5f2d9ae7d2f234e687c886e362c1562f5b2e6
josephhhhhhhhh <e0406453@u.nus.edu>
2020-08-26 04:09:36 +0800

    Level 1. Greet, Echo, Exit has been implemented.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        String awaitCommand;
+        boolean noBye = true;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,6 +14,19 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        while(noBye) {
+            awaitCommand = in.nextLine();
+            if(awaitCommand.equals("bye")){
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(awaitCommand);
+            System.out.println("____________________________________________________________");
+
+        }
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
